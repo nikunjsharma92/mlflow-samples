1672464648e9c0b5f3ee42a76685cb6e62b3799b
Nikunj Sharma <nikunj.sharma92@gmail.com>
2022-07-14 18:35:52 +0530

    version change

--- mlflow-samples/05_spark_cluster_model/hooks.py
+++ mlflow-samples/05_spark_cluster_model/hooks.py
@@ -2,16 +2,19 @@ import json
 from pyspark.sql import SQLContext
 from pyspark import SparkContext
 from pyspark.sql.types import StructType    
+import os
 
 sc = SparkContext.getOrCreate();
 spark = SQLContext(sc)
 
-f = open('schema.json') # Generated at the time of training
+f = open(os.path.join(os.path.dirname(__file__), 'schema.json')) # Generated at the time of training. Always use absolute paths
 schema_json = json.load(f)
 schema = StructType.fromJson(json.loads(schema_json))
 
+schemaReader = spark.read.schema(schema)
+
 def input_fn(X):
-    df = spark.read.schema(schema).json(sc.parallelize([X]))
+    df = schemaReader.json(X)
     return df
 
 def output_fn(Y):
